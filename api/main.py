ad9c7f739d69b495861ce531eb2c7d65c11c86bf
EdgarXMF <91689795+EdgarXMF@users.noreply.github.com>
2023-10-18 21:48:22 +0200

    nginx test

--- api/main.py
+++ api/main.py
@@ -1,6 +1,14 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+import mysql.connector
 app = Flask(__name__)
+
+
+db_connection = mysql.connector.connect(
+    host="db",  # This should match the service name in your Docker Compose file
+    user="root",
+    password="patata",
+    database="granier",
+)
 
 @app.route("/")
 def hello_world():
@@ -9,3 +17,15 @@ def hello_world():
 @app.route("/testing")
 def test():
     return "<p>This is a test</p>"
+
+@app.route('/login', methods=['POST'])
+def process_frontend_data():
+    data = request.json  # Assuming the frontend sends JSON data
+
+    cursor = db_connection.cursor()
+    # Perform database operations using the cursor
+
+    return jsonify({"message": "Data processed successfully"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
