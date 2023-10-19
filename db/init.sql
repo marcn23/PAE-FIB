cc1178154bff8f5601d37964624e979100a3aefe
Vikoey04 <vikoey04@gmail.com>
2023-10-19 09:29:29 +0200

    Fix init.sql syntax error

--- db/init.sql
+++ db/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE events (
     H2 TIME,
     H3 TIME,
     Poblacio VARCHAR(255) NOT NULL,
-    Provinicia VARCHAR(255) NOT NULL,
+    Provincia VARCHAR(255) NOT NULL,
     Lloc VARCHAR(255) NOT NULL,
     LlocMalTemps VARCHAR(255),
     Agrupacio VARCHAR(255) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE events (
     Comentaris VARCHAR(1024),
     Font1_Tipus VARCHAR(255),
     Font1_Direccio VARCHAR(255)
-)
+);
 
 CREATE TABLE users (
     Id INT AUTO_INCREMENT PRIMARY KEY,
@@ -34,10 +34,10 @@ CREATE TABLE users (
     Correu VARCHAR(100) UNIQUE NOT NULL,
     Nif INT(8) UNIQUE NOT NULL,
     Domicili VARCHAR(255) NOT NULL,
-    DomiciliNum INT(4) NOT NULL
+    DomiciliNum INT(4) NOT NULL,
     Poblacio VARCHAR(255) NOT NULL,
     Provincia VARCHAR(255) NOT NULL,
     CodiPostal VARCHAR(6) NOT NULL,
     Telefon INT(9) NOT NULL,
     CodiSgae VARCHAR(10)
-)
+);
